91d3e00a4ef4b04ade8ed8a3372db43d1b379e06
jdevcl <javieralexisparra@clickhost.cl>
2022-10-25 17:14:41 -0300

    Update

--- includes/setup.php
+++ includes/setup.php
@@ -82,6 +82,7 @@ class Theme extends Site
 	{
  		wp_enqueue_style( 'bathe-main', get_theme_file_uri( 'assets/css/main.css' ) );
 		wp_enqueue_style( 'tailwind-main', get_stylesheet_directory_uri() . '/assets/css/tailwind.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/tailwind.css' ) );
+		wp_enqueue_style( 'owl-carousel', get_theme_file_uri( 'assets/dist/owl.carousel.min.css' ) );
 
 		wp_enqueue_script('bathe-bundle', get_theme_file_uri('/assets/js/main.js'), array(), filemtime(get_theme_file_path('/assets/js/main.js')));
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
     theme: {
       extend: {},
       screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
         '2xl': '1690px',
       }
     },
