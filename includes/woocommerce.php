2fea4078b15750b109eb53d3fbe02a8429c6e305
jdevcl <javieralexisparra@clickhost.cl>
2022-10-29 15:32:50 -0300

    Update

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -30,7 +30,11 @@ class WooCommerceTheme {
 	public function hooks() {
 		remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10,0);
 		remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10,0);
-
+		remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10,0);
+		remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10,0);
+		remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+		remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+		remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
 	}
 
 	/**
