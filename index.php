58f07b1250eca9f398c2418cfda9d3325a202b70
jdevcl <javieralexisparra@clickhost.cl>
2022-11-01 23:21:13 -0300

    Update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $context = Timber::context();
 $context['post'] = new TimberPost();
 
 $top_selling = [ 'post_type' => 'product', 'posts_per_page' => 4, 'post_status' => 'publish' ];
-$context['top_selling'] =  Timber::get_posts    ($top_selling);
+$context['top_selling'] =  Timber::get_posts($top_selling);
+$context['category'] = Timber::get_terms('product_cat');
 
 Timber::render('index.twig', $context);
