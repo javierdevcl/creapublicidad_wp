1a30d627035b6223167457a51f1c930a06f1d4bc
Javier Alexis Parra <javieralexisparra@clickhost.cl>
2022-11-05 21:41:53 -0300

    Update

--- assets/dist/owl.js
+++ assets/dist/owl.js
@@ -1,15 +1,3 @@
-// Passive event listeners
-jQuery.event.special.touchstart = {
-  setup: function( _, ns, handle ) {
-    this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
-  }
-};
-jQuery.event.special.touchmove = {
-  setup: function( _, ns, handle ) {
-    this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
-  }
-};
-
 $(document).ready(function() {
   $('.owl-categorias').owlCarousel({
     margin: 10,
