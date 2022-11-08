38b60dc7454efb9b3ccc18bc1dfd86b11fa72a32
jdevcl <javieralexisparra@clickhost.cl>
2022-11-08 15:25:03 -0300

    Update

--- request-quote.php
+++ request-quote.php
@@ -9,29 +9,31 @@
  *
  * @var $template_part string
  * @var $raq_content
- */
-
-/**
- * This file belongs to the YIT Plugin Framework.
- *
- * This source file is subject to the GNU GENERAL PUBLIC LICENSE (GPL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.gnu.org/licenses/gpl-3.0.txt
+ * @var $args
  */
 
 global $wpdb, $woocommerce;
 
 function_exists( 'wc_nocache_headers' ) && wc_nocache_headers();
+
 $quote_wrapper_class = get_option( 'ywraq_page_list_layout_template', '' );
-$quote_wrapper_class = count( $raq_content ) === 0 ? '' : $quote_wrapper_class;
+$show_form           = ( 'yes' === $args['show_form'] ) && ( 'yes' === get_option( 'ywraq_show_form_with_empty_list', 'no' ) || count( $raq_content ) > 0 );
+$main_wrapper_class  = count( $raq_content ) === 0 ? 'ywraq-empty' : '';
+$main_wrapper_class  .= $show_form ? ' ywraq-with-form' : '';
+
+$shop_url             = ywraq_get_return_to_shop_url();
+$label_return_to_shop = apply_filters( 'yith_ywraq_return_to_shop_label', get_option( 'ywraq_return_to_shop_label' ) );
 ?>
 
-<div class="woocommerce ywraq-wrapper">
+<div class="woocommerce ywraq-wrapper <?php echo esc_attr( $main_wrapper_class ); ?>">
 
-    <h2>Resumen de Cotización</h2>
+	<?php if ( $show_form ) : ?>
+		<h2>Resumen de Cotización</h2>
+	<?php endif ?>
 
 	<?php
+
+	// Denied Access.
 	if ( ! apply_filters( 'yith_ywraq_before_print_raq_page', true ) ) :
 		?>
 		<div
@@ -40,6 +42,7 @@ $quote_wrapper_class = count( $raq_content ) === 0 ? '' : $quote_wrapper_class;
 		return;
 	endif;
 
+	// show notices.
 	if ( function_exists( 'wc_print_notices' ) ) {
 		if ( defined( 'YWMMQ_PREMIUM' ) && YWMMQ_PREMIUM ) {
 			wc_print_notices();
@@ -49,31 +52,42 @@ $quote_wrapper_class = count( $raq_content ) === 0 ? '' : $quote_wrapper_class;
 		yith_ywraq_print_notices();
 	}
 
-	if ( ! isset( $_REQUEST['hidem'] ) ) :
+	// from checkout.
+	$quote_id = isset( $_GET['quote_id'] ) ? wp_unslash( $_GET['quote_id'] ) : false; //phpcs:ignore
+	if ( $quote_id && wc_get_order( $quote_id ) && isset( $_REQUEST['hidem'] ) && isset( $_GET['order'] ) ) { //phpcs:ignore
+		$shortcode = '[yith_ywraq_single_view_quote order_id="' . $quote_id . '"]';
+		echo wp_kses_post( is_callable( 'apply_shortcodes' ) ? apply_shortcodes( $shortcode ) : do_shortcode( $shortcode ) );
+	}
+
+
+	if ( ! isset( $_REQUEST['hidem'] ) ) : //phpcs:ignore
 		?>
 		<div id="yith-ywraq-message"><?php do_action( 'ywraq_raq_message' ); ?></div>
 		<?php
-		if ( isset( $_GET['raq_nonce'] ) ) {
+		if ( isset( $_GET['raq_nonce'] ) ) { //phpcs:ignore
 			return;
 		}
 		?>
 
 		<?php
 		if ( get_option( 'ywraq_show_return_to_shop' ) === 'yes' && count( $raq_content ) !== 0 ) :
-			$shop_url             = apply_filters( 'yith_ywraq_return_to_shop_url', get_option( 'ywraq_return_to_shop_url' ) );
-			$label_return_to_shop = apply_filters( 'yith_ywraq_return_to_shop_label', get_option( 'ywraq_return_to_shop_label' ) );
 			?>
-			<div class="yith-ywraq-before-table"><a class="button wc-backward"
-			                                        href="<?php echo esc_url( apply_filters( 'yith_ywraq_return_to_shop_url', $shop_url ) ); ?>"><?php echo esc_html( $label_return_to_shop ); ?></a>
+			<div class="yith-ywraq-before-table"><a class="button wc-backward yith-ywraq-before-table-wc-backward"
+													href="<?php echo esc_url( apply_filters( 'yith_ywraq_return_to_shop_url', $shop_url ) ); ?>"><?php echo esc_html( $label_return_to_shop ); ?></a>
 			</div>
 		<?php endif ?>
 		<div class="ywraq-form-table-wrapper <?php echo esc_attr( $quote_wrapper_class ); ?>">
 			<?php wc_get_template( 'request-quote-' . $template_part . '.php', $args, '', YITH_YWRAQ_TEMPLATE_PATH . '/' ); ?>
 
-			<?php if ( 'yes' == $args['show_form'] && count( $raq_content ) != 0 ) : ?>
+			<?php if ( $show_form ) : ?>
 				<?php if ( ! defined( 'YITH_YWRAQ_PREMIUM' ) ) : ?>
 					<?php wc_get_template( 'request-quote-form.php', $args, '', YITH_YWRAQ_TEMPLATE_PATH . '/' ); ?>
-				<?php else : ?>
+				<?php
+				else :
+					if ( 'default' !== $args['form_type'] && ! empty( $args['form_title'] ) ) {
+						echo '<h3 class="ywraq-form-title">' . wp_kses_post( $args['form_title'] ) . '</h3>';
+					}
+					?>
 					<?php YITH_Request_Quote_Premium()->get_inquiry_form( $args ); ?>
 				<?php endif ?>
 			<?php endif ?>
