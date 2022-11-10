558d75b89d978ed00fce3c367968d652330d08df
jdevcl <javieralexisparra@clickhost.cl>
2022-11-09 22:58:33 -0300

    Update

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -28,7 +28,7 @@ if ( $related_products ) : ?>
 
 		if ( $heading ) :
 			?>
-			<h2 class="text-lg uppercase border-b border-black mb-4 md:mb-7"><?php echo esc_html( $heading ); ?></h2>
+			<span class="text-lg uppercase border-b border-black mb-4 md:mb-7"><?php echo esc_html( $heading ); ?></span>
 		<?php endif; ?>
 
 		<ul class="owl-carousel owl-relacionados">
